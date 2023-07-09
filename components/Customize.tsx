9440ae70df3b0b114cd58e241802d588c2e1328b
Pritish <pritishmishra579@gmail.com>
2023-07-09 15:14:45 +0530

    &apos; change

--- components/Customize.tsx
+++ components/Customize.tsx
@@ -21,7 +21,7 @@ export function Customize() {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
