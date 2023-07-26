9623f53d968fe21659945fc040e152186fb5332a
Pritish <pritishmishra579@gmail.com>
2023-07-26 17:10:55 +0530

    redundant code del

--- components/Metor.tsx
+++ components/Metor.tsx
@@ -74,7 +74,6 @@ const Meteors = () => {
           </radialGradient>
         </defs>
       </svg>
-      {/* <div className="pointer-events-none h-full absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> */}
     </div>
   );
 };
