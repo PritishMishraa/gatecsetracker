f9a28152f3ec33b07da7b5a6dcbcce53be40de58
Pritish <pritishmishra579@gmail.com>
2023-07-30 21:36:44 +0530

    new UI for Subject Page

--- app/subject/page.tsx
+++ app/subject/page.tsx
@@ -1,21 +1,36 @@
-import SubjectCard from "@/components/SubjectCard";
+import { Subjects } from "@/components/Subjects";
 
 const Home = () => {
   const subjects = require("../../data/subjects.json");
 
   return (
-    <div className="max-w-2xl mx-auto mb-20 p-4 md:p-0">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-12">
-        {subjects.map(
-          (subject: { subjectCode: string; subjectName: string }) => (
-            <SubjectCard
-              key={subject.subjectCode}
-              subjectCode={subject.subjectCode}
-              subjectName={subject.subjectName}
-            />
-          )
-        )}
+    <div className="container flex flex-col items-center justify-center gap-12 mt-6 md:mt-10 w-full">
+      <div className="relative isolate overflow-hidden mt-12 bg-primary-foreground border px-4 md:px-8 py-12 text-center rounded-3xl w-full">
+        <h1 className="text-4xl md:text-7xl font-bold text-left">Subjects</h1>
+        <p className="text-xl md:text-2xl text-left mt-2 text-white/60">
+          Select a subject to get started
+        </p>
+        <svg
+          viewBox="0 0 1024 1024"
+          className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+          aria-hidden="true"
+        >
+          <circle
+            cx="512"
+            cy="512"
+            r="512"
+            fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
+            fillOpacity="0.7"
+          ></circle>
+          <defs>
+            <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+              <stop stopColor="#0061ff"></stop>
+              <stop offset="1" stopColor="#60efff"></stop>
+            </radialGradient>
+          </defs>
+        </svg>
       </div>
+      <Subjects />
     </div>
   );
 };
---
+++ components/Subjects.tsx
@@ -0,0 +1,141 @@
+import {
+  FunctionSquare,
+  Settings,
+  Network,
+  Database,
+  Code,
+  Cpu,
+  MonitorUp,
+  BarChart4,
+  Box,
+  CheckSquare,
+  Infinity,
+  FileTerminal,
+  ReplaceAll,
+  FileTerminalIcon,
+  InfinityIcon,
+} from "lucide-react";
+
+import CategoryCard from "./category-card";
+
+const SUBJECTS: {
+  title: string;
+  slug: string;
+  description: string;
+  icon: JSX.Element;
+}[] = [
+  {
+    title: "Discrete Mathematics",
+    slug: "dm",
+    description:
+      "This subject covers the study of mathematical structures that are fundamentally discrete rather than continuous.",
+    icon: <FunctionSquare className="h-6 w-6" />,
+  },
+  {
+    title: "Theory of Computation",
+    slug: "toc",
+    description:
+      "This subject deals with the study of abstract machines and computational problems.",
+    icon: <Settings className="h-6 w-6" />,
+  },
+  {
+    title: "Computer Network",
+    slug: "cn",
+    description:
+      "This subject focuses on the study of data communication and computer networking.",
+    icon: <Network className="h-6 w-6" />,
+  },
+  {
+    title: "Database Management System",
+    slug: "dbms",
+    description:
+      "This subject covers the study of managing and organizing data in a database.",
+    icon: <Database className="h-6 w-6" />,
+  },
+  {
+    title: "C Programming",
+    slug: "cp",
+    description:
+      "This subject involves learning the C programming language and its applications.",
+    icon: <Code className="h-6 w-6" />,
+  },
+  {
+    title: "Digital Logic",
+    slug: "dl",
+    description:
+      "This subject focuses on the study of digital circuits and logic gates.",
+    icon: <Cpu className="h-6 w-6" />,
+  },
+  {
+    title: "Operating System",
+    slug: "os",
+    description:
+      "This subject covers the study of computer operating systems and their components.",
+    icon: <MonitorUp className="h-6 w-6" />,
+  },
+  {
+    title: "Computer Organization & Architecture",
+    slug: "coa",
+    description:
+      "This subject involves the study of the structure and organization of computer systems.",
+    icon: <BarChart4 className="h-6 w-6" />,
+  },
+  {
+    title: "Data Structure",
+    slug: "ds",
+    description:
+      "This subject covers the study of data organization and efficient algorithms.",
+    icon: <Box className="h-6 w-6" />,
+  },
+  {
+    title: "Aptitude",
+    slug: "a",
+    description:
+      "This subject involves testing the problem-solving and reasoning skills of individuals.",
+    icon: <CheckSquare className="h-6 w-6" />,
+  },
+  {
+    title: "Engineering Mathematics",
+    slug: "em",
+    description:
+      "This subject covers the study of mathematical concepts and their applications in engineering.",
+    icon: <InfinityIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Compiler Design",
+    slug: "cd",
+    description:
+      "This subject covers the study of designing and building a compiler.",
+    icon: <FileTerminalIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Algorith",
+    slug: "algo",
+    description:
+      "This subject covers the study of designing and building algorithms.",
+    icon: <ReplaceAll className="h-6 w-6" />,
+  },
+];
+
+export const Subjects = () => {
+  return (
+    <div className="not-prose grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {SUBJECTS.map((subject) => (
+        <CategoryCard
+          key={subject.slug}
+          slug={subject.slug}
+          name={subject.title}
+          description={subject.description}
+          icon={subject.icon}
+          pattern={{
+            y: 16,
+            squares: [
+              [0, 1],
+              [1, 3],
+            ],
+          }}
+        />
+      ))}
+    </div>
+  );
+};
