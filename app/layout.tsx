58af5f92aec4aacdfc34427d14154e00209ca466
Pritish <pritishmishra579@gmail.com>
2023-08-03 16:12:00 +0530

    feat: OG tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:url" content="https://gatecsetracker.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="GATE CSE Tracker" />
+        <meta
+          property="og:description"
+          content="Track and Achieve Success in the GATE CSE Exam"
+        />
         <meta
           property="og:image"
           content="https://i.postimg.cc/brn0WTcz/GCT-OG-Image.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="gatecsetracker.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://gatecsetracker.vercel.app/"
+        />
+        <meta name="twitter:title" content="GATE CSE Tracker" />
+        <meta
+          name="twitter:description"
+          content="Track and Achieve Success in the GATE CSE Exam"
         />
         <meta
           name="twitter:image"
