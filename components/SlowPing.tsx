abf79b78391a2032ee54c4cbba75993e7e101d2f
Pritish <pritishmishra579@gmail.com>
2023-08-10 23:31:09 +0530

    feat: new component

---
+++ components/SlowPing.tsx
@@ -0,0 +1,37 @@
+const SlowPing = () => {
+  return (
+    <svg
+      className="animate-slow h-5 w-5"
+      viewBox="0 0 50 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M30.5 25C30.5 28.0376 28.0376 30.5 25 30.5C21.9624 30.5 19.5 28.0376 19.5 25C19.5 21.9624 21.9624 19.5 25 19.5C28.0376 19.5 30.5 21.9624 30.5 25Z"
+        stroke="currentColor"
+        stroke-opacity="0.7"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+      <path
+        d="M38.75 25C38.75 32.5939 32.5939 38.75 25 38.75C17.4061 38.75 11.25 32.5939 11.25 25C11.25 17.4061 17.4061 11.25 25 11.25C32.5939 11.25 38.75 17.4061 38.75 25Z"
+        stroke="currentColor"
+        stroke-opacity="0.4"
+        stroke-width="4.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+      <path
+        d="M47.5 25C47.5 37.4264 37.4264 47.5 25 47.5C12.5736 47.5 2.5 37.4264 2.5 25C2.5 12.5736 12.5736 2.5 25 2.5C37.4264 2.5 47.5 12.5736 47.5 25Z"
+        stroke="currentColor"
+        stroke-opacity="0.1"
+        stroke-width="5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default SlowPing;
