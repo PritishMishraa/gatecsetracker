8245f50294dcfc21a6b50496873c687130a0b308
Pritish Mishra <pritishmishra579@gmail.com>
2024-08-25 11:27:19 +0530

    refactor: :speech_balloon: new announcement

--- components/Announcment.tsx
+++ components/Announcment.tsx
@@ -9,12 +9,13 @@ const Announcment = () => {
           className="flex flex-col sm:flex-row py-2.5 sm:items-center w-full justify-between"
         >
           <p>
-            Introducing{" "}
-            <span className="font-bold tracking-wider">Sub-Req</span> for
-            Students
+            Introducing
+            <span className="font-bold tracking-wider"> Sub-Req </span>
+            for Students
           </p>
           <p>
-            Join the waitlist for early access &nbsp;
+            Request your ❤️ YouTube playlist
+            <span className="font-bold tracking-wider"> NOW </span> &nbsp;
             <span aria-hidden="true">→</span>
           </p>
         </Link>
