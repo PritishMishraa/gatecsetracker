7d93f2bc6ea07ff3e3a5b7e283e69b0f0a4ef8e8
Pritish Mishra <pritishmishra579@gmail.com>
2026-04-01 15:50:06 +0530

    Add Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,18 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier/flat";
 
-const config = [...nextCoreWebVitals];
+const config = [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "dist/**",
+      "tsconfig.tsbuildinfo",
+    ],
+  },
+  ...nextCoreWebVitals,
+  prettier,
+];
 
 export default config;
